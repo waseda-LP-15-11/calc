3eb9df96e0cb84446813bff705768f80305ea88b
keigo-brook <goog@brookbach.com>
2015-05-05 22:12:48 +0900

    set max 15 digits

--- arithmetic.c
+++ arithmetic.c
@@ -1,10 +1,13 @@
-#include <float.h>
 #include <stdio.h>
 #include <math.h>
 #include "arithmetic.h"
 
+// doubleの精度が１５桁なのでMAXを１６桁にする
+# define MAX 1000000000000000
+# define MIN -1000000000000000
+
 double add(double a, double b) {
-	if (((a > 0) && (b > (DBL_MAX - a))) || ((b < 0) && (a < (DBL_MIN - a)))){
+	if (((a > 0) && (b > (MAX - a))) || ((b < 0) && (a < (MIN - a)))){
 		printf("ERROR:: Overflow\n");
 		return 0;
 	}
@@ -13,7 +16,7 @@ double add(double a, double b) {
 }
 
 double sub(double a, double b) {
-	if ((a > 0 && b < DBL_MIN + a) || (a < 0 && b > DBL_MAX + a)) {
+	if ((a > 0 && b < MIN + a) || (a < 0 && b > MAX + a)) {
 		printf("ERROR:: Overflow\n");
 		return 0;
 	}
@@ -24,24 +27,24 @@ double sub(double a, double b) {
 double multiple(double a, double b) {
 	if (a > 0) {  
 		if (b > 0) {  
-			if (a > (DBL_MAX / b)) {
+			if (a > (MAX / b)) {
 				printf("ERROR:: Overflow\n");
 				return 0;        
 			}
     } else { 
-			if (b < (DBL_MIN / a)) {
+			if (b < (MIN / a)) {
 				printf("ERROR:: Overflow\n");
 				return 0;
 			}
 		} 
 	} else { 
 		if (b > 0) {
-			if (a < (DBL_MIN / b)) {
+			if (a < (MIN / b)) {
 				printf("ERROR:: Overflow\n");
 				return 0;
 			}
 		} else { 
-			if ( (a != 0) && (b < (DBL_MAX / a))) {
+			if ( (a != 0) && (b < (MAX / a))) {
 				printf("ERROR:: Overflow\n");
 				return 0;
 			}
@@ -53,19 +56,14 @@ double multiple(double a, double b) {
 
 double divide(double a, double b) {
 	if (b == 0) {
-		printf("can't divide by 0\n");
+		printf("ERROR: Can't divide by 0\n");
 		return 0;
 	}
-	if (((a == DBL_MIN) && (b == -1))) {
-		printf("ERROR: Overflow\n");
-		return 0;
-	} 
-
 	return a / b;
 }
 
 double pow_with_check_overflow(double a, double b) {
-	if (b > log(DBL_MAX)/log(a)) {
+	if (b > log(MAX)/log(a)) {
 		printf("ERROR: Overflow\n");
 		return 0;
 	}
